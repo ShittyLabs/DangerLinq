ec804e03e34109d2654cecdacdb538a3654484bd
Johnny Reina <johnny.r.reina@gmail.com>
2020-10-06 07:48:32 -0500

    Adds Aggregate<T, U> (#2)
    Fixes #1

--- src/DangerLinqFunctionMapping.ts
+++ src/DangerLinqFunctionMapping.ts
@@ -2,6 +2,9 @@ import { DangerLinqFunction } from "./DangerLinqFunction";
 import { DontCallThisError } from "./Errors/DontCallThisError";
 
 export const DangerLinqFunctionMapping = {
+  [DangerLinqFunction.Aggregate]: <T, U>(xs: Array<T>, seed: U, reducer: ((memo: U, val: T) => U)): U => {
+    throw new DontCallThisError();
+  },
   [DangerLinqFunction.Map]: <T, U>(arr: Array<T>, xform: (x: T) => U): Array<U> => {
     throw new DontCallThisError();
   },
