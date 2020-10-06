fbbdfaef7916416d6a035ee8acd43e6d6e020a16
Johnny Reina <johnny.r.reina@gmail.com>
2020-10-05 19:00:52 -0500

    Adds aggregate1 Fixes #2

--- src/DangerLinqFunction.ts
+++ src/DangerLinqFunction.ts
@@ -1,4 +1,5 @@
 export enum DangerLinqFunction {
+    Aggregate1 = "jreina/0f4dab07c6be88054df6de0f350d3f76/raw/84116870af9aa3c42997c89b1240d4ad6d85c095/Aggregate1.js",
     Map = "jreina/8c80198ae0048257e50ac80e830f8055/raw/65122af3f7fa0d0d8bbc7d4809b3c622085cd9b7/Map.js",
     SleepSort = "jreina/a90a94fdd6cc68a6cf4270efcfbe21bf/raw/e2ba064cc7c524ee96e0fe05dd4028ff3764fa4c/sleep-sort.js"
 }
--- src/DangerLinqFunctionMapping.ts
+++ src/DangerLinqFunctionMapping.ts
@@ -2,6 +2,9 @@ import { DangerLinqFunction } from "./DangerLinqFunction";
 import { DontCallThisError } from "./Errors/DontCallThisError";
 
 export const DangerLinqFunctionMapping = {
+  [DangerLinqFunction.Aggregate1]: <T>(xs: Array<T>, reducer: (memo: T, val: T) => T): T => {
+    throw new DontCallThisError();
+  },
   [DangerLinqFunction.Map]: <T, U>(arr: Array<T>, xform: (x: T) => U): Array<U> => {
     throw new DontCallThisError();
   },
