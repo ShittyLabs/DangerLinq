ae7e2dd3c1b5eef8e54c2b988b65e028c8e4b755
Patrick Tone <patrick@madronesoftware.com>
2020-10-18 09:15:56 +0200

    Adds `Batch`
    Batch gist: https://gist.githubusercontent.com/the-pat/d5a1bfca21be2beafad0a6a7559013e0/raw/a994a899302f07475ebb99041ac9d4c53aa81776/Batch.js
    Batch1 gist: https://gist.githubusercontent.com/the-pat/f7588fca09549c30de33808a900cc60f/raw/1532ea284bac98acd72a4ff3d49b203969def63d/Batch1.js

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import got from "got";
 
 import { DangerLinqFunction } from "./DangerLinqFunction";
 import { DangerLinqFunctionMapping } from "./DangerLinqFunctionMapping";
-import { Grouping as grouping } from './Types/Grouping';
+import { Grouping as grouping } from "./Types/Grouping";
 const Grouping = grouping;
 
 const cache = new Map<DangerLinqFunction, Function>();
@@ -13,6 +13,10 @@ export function DangerLinq(func: DangerLinqFunction.Aggregate): Promise<typeof D
 export function DangerLinq(func: DangerLinqFunction.Aggregate1): Promise<typeof DangerLinqFunctionMapping[DangerLinqFunction.Aggregate1]>;
 /** Applies an accumulator function over every element in the source array using the specified seed value as the initial memo value, and applies the result selector to the result. */
 export function DangerLinq(func: DangerLinqFunction.Aggregate2): Promise<typeof DangerLinqFunctionMapping[DangerLinqFunction.Aggregate2]>;
+/** Applies a function to batch the source sequence into sized buckets. */
+export function DangerLinq(func: DangerLinqFunction.Batch): Promise<typeof DangerLinqFunctionMapping[DangerLinqFunction.Batch]>;
+/** Applies a function to batch the source sequence into sized buckets and applies a projection to each bucket. */
+export function DangerLinq(func: DangerLinqFunction.Batch1): Promise<typeof DangerLinqFunctionMapping[DangerLinqFunction.Batch1]>;
 /** Applies a function to each value in the input array and groups the array values according to the result. */
 export function DangerLinq(func: DangerLinqFunction.GroupBy): Promise<typeof DangerLinqFunctionMapping[DangerLinqFunction.GroupBy]>;
 /** Creates a new array by applying the supplied function to every element in the source array. */
